f18895cdf91ddaaef39dea2a1869077becd6252e
JackB91 <you@example.com>
2025-04-04 20:22:11 -0700

    typewriter animation for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 export default function Header () {
-const [welcome,setWelcome] = useState("")
+    const welcomeMsg = "Welcome to the Memory Game"
+    const [welcome, setWelcome] = useState("")
+    const [index ,setIndex] = useState(0)
+
+    useEffect(() => {
+        if (index < welcomeMsg.length) {
+            const timeout = setTimeout(() => {
+                setWelcome(prev => prev + welcomeMsg[index])
+                setIndex(prev => prev + 1)
+            },150)
+
+            
+
+            return () => clearTimeout(timeout)
+        }
+
+        else {
+            const resetTimeout = setTimeout(() => {
+                setWelcome("") 
+                setIndex(0)
+
+            },2000)
+        }
+    }, [index, welcomeMsg])
+
 
     return (
         <header className="header">
-            <h1>Welcome!</h1>
+            <h1>{welcome}</h1>
         </header>
     )
 }
